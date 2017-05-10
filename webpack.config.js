ef5fde551a7178fc0b68cd8404dff4e0eb801ceb
Jason Vance <jbvance@gmail.com>
2017-05-10 12:19:28 -0500

    add Webpack functionality

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,49 @@
 var webpack = require('webpack');
 var path = require('path');
 
+//remove the need to manually include <script> tags in index.html
+// this plugin automatically produces an html doc that has the correct
+// script tags insdie of it
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const VENDOR_LIBS = [
+  'react', 'lodash', 'redux', 'react-redux', 'react-dom',
+  'faker', 'react-input-range', 'redux-form', 'redux-thunk'
+];
+
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    bundle: './src/index.js',
+    vendor: VENDOR_LIBS
+  },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
-  }
+    // Uniquely name each file so that it will
+    // "bust" the user's cache and force a new download
+    // of the file if it has been updated since the user
+    // last visited the site
+    filename: '[name].[chunkhash].js'
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      },
+      {
+        use: ['style-loader', 'css-loader'],
+        test: /\.css$/
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['vendor', 'manifest']
+    }),
+    //the below plugins add <script> tags to the html doc specified
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ]
 };
